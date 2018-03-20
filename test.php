244228a1ac4cc31077d543dd8146d55deb414971
Ta Phuoc Anh <taphuocanh@huaf.edu.vn>
2018-03-20 13:35:17 +0700

    add

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+
+    $config = [ 'host'     => '10.6.1.57',
+                'port'     => '3306',
+                'username' => 'root',
+                'password' => 'password',
+                'database' => 'qlgd' 
+            ];
+            
+
+    
+    $conn = new mysqli($config['host'], $config['username'], $config['password'], $config['database']);
+    mysqli_set_charset($conn,"utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    if ($result = $conn->query("SELECT * FROM ctdt_tu_dien_mon_hoc")) {
+        $data = [];
+        
+        while ($myrow = $result->fetch_array(MYSQLI_ASSOC))
+        {
+            $data[] = $myrow;
+        }
+
+        echo "<pre>";
+        print_r($data);
+        $result->close();
+    }
+    
+
+    $conn->close();
+
+//var_dump($conn);
+
+        
